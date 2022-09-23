ba7d266521816f1cf05e6e246c17b9d96bab9395
Selestine <selemtei@gmail.com>
2022-09-23 17:07:38 +0300

    Updates

---
+++ _next/static/nlrX2iPeydA27biP1yJy5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/828-9614e2acd1907056.js","static/chunks/pages/index-bcc7bc4d378f5e3b.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
